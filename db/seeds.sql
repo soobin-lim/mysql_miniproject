78ac2b97d7abfa22623085b2175a257c123f066a
Soobin Lim <bootcamp@Soobins-Mac-mini.local>
2021-09-11 13:31:09 -0500

    firstcommit

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO movies (price)
+VALUES ('movie1'),
+       ('movie2'),
+       ('movie3'),
+       ('movie4'),
+       ('movie5');
+
+INSERT INTO favorite_books (book_name, in_stock, book_price)
+VALUES ("The Great Gatsby", true, 1),
+       ("Huckleberry Finn", true, 3),
+       ("100 Years of Solitude", false, 5),
+       ("Things Fall Apart", false, 1),
+       ("Crime and Punishment", true, 2),
+       ("Moby Dick", true, 4),
+       ("Decameron", false, 1);
